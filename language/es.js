d490774e72754e93f67e17c548340c923b9d3b2f
maulana3012 <muhamadmaulanarachman@gmail.com>
2020-06-05 21:04:32 +0700

    update-1

---
+++ language/es.js
@@ -0,0 +1,124 @@
+var Translations = {
+    "SkipBackButtonText": "Atrás",
+    "SkipForwardButtonText": "Avanzar",
+    "PlayPauseButtonText": "Reproducir/Pausar",
+    "VolumeButtonText": "Volumen",
+    "SettingsButtonText": "Ajustes",
+    "ResumeVideoPlayback": "Reanudar la reproducción de vídeo",
+
+    "QualityMenuText": "Calidad",
+    "QualityAutoText": "Auto",
+    "LanguageMenuText": "Idioma",
+    "CCMenuText": "Subtítulos",
+    "BackFromText": "Back from",
+    "OnText": "On",
+    "OffText": "Off",
+
+    "SocialIcon-facebook": "Facebook",
+    "SocialIcon-twitter": "Twitter",
+    "SocialIcon-googleplus": "Google+",
+    "SocialIcon-email": "Email",
+    "SocialIcon-link": "Website",
+    "CollapseText": "Colapso",
+    "ExpandText": "Expandir",
+
+    "PreparedForText": "Preparado para",
+
+    "CopyrightText": "Copyright " + Utils.getFullYear() + " Client Name",
+    "PoweredByFooterText": "Powered by",
+    "HelpFooterText": "Ayuda",
+    "PrivacyPolicyFooterText": "Política de privacidad",
+
+    /*
+     * See en.js for details
+     */
+    "CtaButtonSettings": {
+        "button1": {
+            "card": "7b480c70",
+            "label": "Botón 1",
+            "icon": "contact"
+        },
+        "button2": {
+            "card": "7b480c70",
+            "label": "Botón 2",
+            "icon": "website"
+        },
+        "button3": {
+            "card": "7b480c70",
+            "label": "Botón 3",
+            "icon": "email"
+        },
+        "button4": {
+            "card": "7b480c70",
+            "label": "Botón 4",
+            "icon": "save"
+        },
+        "button5": {
+            "url": "http://example.com/es",
+            "label": "Botón 5",
+            "icon": "share"
+        }
+    },
+
+    /*
+     * See en.js for details
+     */
+    "PromoSettings": {
+        "promo1": {
+            "trackingName": "promo1",
+            "url": "http://example.com/es",
+            "label": "Promo (Top)",
+            "desktopImage": "./images/promo_portrait.png",
+            "mobileImage": "./images/promo_landscape.png"
+        },
+        "promo2": {
+            "trackingName": "promo2",
+            "url": "http://example.com/es",
+            "label": "Promo (Bottom)",
+            "desktopImage": "./images/promo_portrait.png",
+            "mobileImage": "./images/promo_landscape.png"
+        }
+    },
+
+    /*
+     * See en.js for details
+     */
+    "ChapterSettings": [
+        {
+            "label": "Selamat Datang",
+            "states": [
+                {
+                    "cardId": "START",
+                    "width": 25
+                }
+            ]
+        },
+        {
+            "label": "Informasi Polis",
+            "states": [
+                {
+                    "cardId": "77a834f0",
+                    "width": 25
+                }
+            ]
+        },
+        {
+            "label": "Konfirmasi Polis",
+            "states": [
+                {
+                    "cardId": "7b0123a5",
+                    "width": 25
+                }
+            ]
+        },
+        {
+            "label": "Terima Kasih",
+            "states": [
+                {
+                    "cardId": "7b0123a5",
+                    "width": 25
+                }
+            ]
+        }
+    ]
+}
